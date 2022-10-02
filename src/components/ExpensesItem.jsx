fd31a9637c436d96599cd4aad684c6ffbc6f251e
Achref <medachrefg@gmail.com>
2022-10-02 13:34:42 +0100

    passing data via props

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './ExpensesItem.css';
-const ExpensesItem = () => {
-	const expenseDate = new Date(2021, 2, 17);
-	const expenseTitle = 'car insurance ';
-	const expenseAmount = 254;
+const ExpensesItem = ({ expenses }) => {
 	return (
 		<>
 			<div className='expense-item'>
-				<div>{expenseDate.toString()}</div>
+				<div>{expenses.date.toString()}</div>
 				<div className='expense-item__description'>
-					<h2> {expenseTitle}</h2>
-					<div className='expense-item__price '> {expenseAmount}</div>
+					<h2> {expenses.title}</h2>
+					<div className='expense-item__price '> {expenses.amount}</div>
 				</div>
 			</div>
 		</>
