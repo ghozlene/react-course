bf850af56962a731af08281abd0082ad6a2f7b0c
Achref <medachrefg@gmail.com>
2022-10-02 15:03:09 +0100

    using composition on expenses item too

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Expenses.css';
 import ExpensesItem from './ExpensesItem';
+import Card from './Card';
 const Expenses = ({ expenses }) => {
 	return (
-		<div className='expenses'>
+		<Card className='expenses'>
 			<ExpensesItem expenses={expenses[0]} />
 			<ExpensesItem expenses={expenses[1]} />
 			<ExpensesItem expenses={expenses[2]} />
-		</div>
+		</Card>
 	);
 };
 
