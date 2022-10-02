070392296e024ef90f2781ac87f76a2909f2ea49
Achref <medachrefg@gmail.com>
2022-10-02 14:07:58 +0100

    adding normal js logic to component

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './ExpensesItem.css';
 const ExpensesItem = ({ expenses }) => {
+	const month = expenses.date.toLocaleString('en-us', { month: 'long' });
+	const year = expenses.date.getFullYear();
+	const day = expenses.date.toLocaleString('en-us', { day: '2-digit' });
 	return (
 		<>
 			<div className='expense-item'>
+				<div>
+					<div>{month}</div>
+					<div>{day}</div>
+					<div>{year}</div>
+				</div>
+
 				<div>{expenses.date.toString()}</div>
 				<div className='expense-item__description'>
 					<h2> {expenses.title}</h2>
