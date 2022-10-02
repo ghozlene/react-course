dd14c41aa557b37034e88b0e952eb08cff1a23f4
Achref <medachrefg@gmail.com>
2022-10-02 12:57:31 +0100

    adding basic style

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import './ExpensesItem.css';
 const ExpensesItem = () => {
-	return <div>ExpensesItem</div>;
+	return (
+		<>
+			<div className='expense-item'>
+				<div>March 28th 2021</div>
+				<div className='expense-item__description'>
+					<h2> car insurance</h2>
+					<div className='expense-item__price '> 251$</div>
+				</div>
+			</div>
+		</>
+	);
 };
-
 export default ExpensesItem;
